193a950a1211e59986e4e87817dca9cf00c0e959
No3x3 <steelwarrior11cm@gmail.com>
2025-07-18 12:22:16 +0200

    Update cs2.py

--- cs2.py
+++ cs2.py
@@ -1,21 +1,16 @@
 import sys
 import os
 import json
+import glob
+import time
 import requests
 from PyQt5 import QtWidgets, QtGui, QtCore
 
-DEFAULT_LOG_PATH = os.path.expanduser("~/tf2_gamelog.txt")
 GAMESENSE_ADDRESS = "http://localhost:51234"
 GAME_NAME = "CS2"
 
-# Kategorie i eventy TF2 (bez zmian)
+# Kategorie i eventy TF2 (do mapowania)
 TF2_EVENT_CATEGORIES = {
-    "Zdrowie i amunicja": [
-        ("tf2_health", "Pokazuje aktualny poziom zdrowia gracza po otrzymaniu obrażeń lub leczeniu."),
-        ("tf2_overheal", "Pokazuje nadwyżkę zdrowia (overheal) po uzyskaniu leczenia powyżej bazowego poziomu."),
-        ("tf2_ammo", "Pokazuje aktualny stan amunicji po oddaniu strzału, przeładowaniu lub zmianie broni."),
-        ("tf2_armor", "Pokazuje poziom pancerza (jeśli dotyczy klasy/modyfikacji)."),
-    ],
     "Zabójstwa i śmierć": [
         ("tf2_kill", "Aktywowany po zabiciu przeciwnika przez gracza."),
         ("tf2_death", "Aktywowany po śmierci gracza."),
@@ -25,32 +20,23 @@ TF2_EVENT_CATEGORIES = {
         ("tf2_headshot", "Aktywowany po trafieniu przeciwnika w głowę."),
         ("tf2_backstab", "Aktywowany po zadaniu śmiertelnego ciosu z pleców jako szpieg."),
         ("tf2_first_blood", "Aktywowany przy pierwszym zabójstwie w rundzie."),
+        ("tf2_respawn", "Aktywowany po odrodzeniu gracza."),
     ],
-    "Budowle i inżynier": [
-        ("tf2_build", "Aktywowany po zbudowaniu budowli inżyniera."),
-        ("tf2_destroy", "Aktywowany po zniszczeniu budowli inżyniera przez gracza."),
-    ],
-    "Leczenie i Uber": [
-        ("tf2_heal", "Aktywowany po wyleczeniu innego gracza."),
-        ("tf2_ubercharge", "Aktywowany po użyciu ubercharge przez medyka."),
-    ],
-    "Flag/Point": [
+    "Cele mapy": [
         ("tf2_flag_pickup", "Aktywowany po podniesieniu walizki (CTF) przez gracza."),
         ("tf2_flag_capture", "Aktywowany po zdobyciu walizki przez gracza."),
         ("tf2_flag_drop", "Aktywowany po upuszczeniu walizki przez gracza."),
         ("tf2_point_capture", "Aktywowany po przejęciu punktu kontrolnego przez gracza."),
     ],
-    "Pozostałe": [
+    "Inne": [
         ("tf2_bonus", "Aktywowany po zdobyciu bonusu za rundę (np. MVP)."),
         ("tf2_taunt", "Aktywowany po wykonaniu gestu (taunt) przez gracza."),
         ("tf2_vote_cast", "Aktywowany po oddaniu głosu w głosowaniu."),
         ("tf2_vote_pass", "Aktywowany po pozytywnym zakończeniu głosowania."),
-        ("tf2_respawn", "Aktywowany po odrodzeniu gracza."),
         ("tf2_incoming", "Aktywowany po dołączeniu gracza do serwera lub drużyny."),
     ],
 }
 
-# Oficjalne eventy GameSense dla CS2
 GAMESENSE_EVENTS = [
     ("HEALTH", "Stan zdrowia gracza"),
     ("AMMO", "Stan amunicji gracza"),
@@ -66,6 +52,10 @@ GAMESENSE_EVENTS = [
     ("TAUNT", "Wykonanie gestu"),
     ("RESPAWN", "Odrodzenie"),
     ("INCOMING", "Dołączenie do gry"),
+    ("POINT_CAPTURE", "Przejęcie punktu"),
+    ("FLAG_PICKUP", "Podniesienie flagi"),
+    ("FLAG_CAPTURE", "Zdobycie flagi"),
+    ("FLAG_DROP", "Upuszczenie flagi"),
 ]
 
 def register_gamesense():
@@ -88,71 +78,78 @@ def send_gamesense_event(event, value=None):
     except requests.exceptions.RequestException:
         pass
 
+def find_latest_log(logs_folder):
+    log_files = glob.glob(os.path.join(logs_folder, "L*.log"))
+    if not log_files:
+        return None
+    return max(log_files, key=os.path.getmtime)
+
 class LogMonitorThread(QtCore.QThread):
     new_event = QtCore.pyqtSignal(str, object)
-
-    def __init__(self, log_path, event_map):
+    def __init__(self, logs_folder, event_map, my_nick=""):
         super().__init__()
-        self.log_path = log_path
+        self.logs_folder = logs_folder
         self.event_map = event_map
         self.running = True
+        self.current_logfile = None
         self.last_position = 0
-        self.last_health = None
-        self.last_ammo = None
+        self.my_nick = my_nick  # można rozwinąć, by automatycznie wykrywał nick z loga
 
     def run(self):
         while self.running:
-            if not os.path.isfile(self.log_path):
+            latest_log = find_latest_log(self.logs_folder)
+            if not latest_log:
                 self.msleep(500)
                 continue
-            with open(self.log_path, "r", encoding="utf-8", errors="ignore") as f:
-                f.seek(self.last_position)
-                lines = f.readlines()
-                self.last_position = f.tell()
-            for line in lines:
-                self.process_line(line.strip())
+            if latest_log != self.current_logfile:
+                self.current_logfile = latest_log
+                self.last_position = 0
+            try:
+                with open(self.current_logfile, "r", encoding="utf-8", errors="ignore") as f:
+                    f.seek(self.last_position)
+                    lines = f.readlines()
+                    self.last_position = f.tell()
+                for line in lines:
+                    self.process_line(line.strip())
+            except Exception:
+                pass
             self.msleep(100)
 
     def stop(self):
         self.running = False
 
     def process_line(self, line):
-        # Przykładowe reguły (dopasuj do formatu loga TF2 z con_logfile)
-        # HEALTH
-        if "Health:" in line:
-            try:
-                health_str = line.split("Health:")[1].split("/")[0].strip()
-                health_val = int(health_str)
-                if health_val != self.last_health:
-                    self.last_health = health_val
-                    self.emit_event('tf2_health', health_val)
-            except Exception:
-                pass
-        # AMMO
-        elif "Ammo:" in line:
-            try:
-                ammo_str = line.split("Ammo:")[1].split()[0].strip()
-                ammo_val = int(ammo_str)
-                if ammo_val != self.last_ammo:
-                    self.last_ammo = ammo_val
-                    self.emit_event('tf2_ammo', ammo_val)
-            except Exception:
-                pass
+        # Uwaga: poniżej bardzo uproszczone reguły. Dostosuj do swoich logów!
+        # Twój nick (do lepszej filtracji eventów) możesz ustawić w GUI lub tu
+        nick = self.my_nick or "Dawid"  # zmień na swój nick w TF2
+        lline = line.lower()
         # KILL
-        elif "killed" in line and "with" in line:
-            self.emit_event('tf2_kill')
-        # DEATH
-        elif "You died" in line or "died" in line:
-            self.emit_event('tf2_death')
+        if 'killed' in lline and '"' in lline:
+            if f'"{nick}"' in lline:
+                self.emit_event('tf2_kill')
+            elif 'killed' in lline and f'"{nick}"' not in lline:
+                self.emit_event('tf2_death')
         # HEADSHOT
-        elif "headshot" in line:
+        if 'headshot' in lline:
             self.emit_event('tf2_headshot')
         # BACKSTAB
-        elif "backstab" in line:
+        if 'backstab' in lline:
             self.emit_event('tf2_backstab')
-        # RESPAWN
-        elif "respawned" in line or "has respawned" in line:
-            self.emit_event('tf2_respawn')
+        # FLAG_PICKUP
+        if 'picked up the intelligence' in lline:
+            self.emit_event('tf2_flag_pickup')
+        # FLAG_CAPTURE
+        if 'captured the intelligence' in lline:
+            self.emit_event('tf2_flag_capture')
+        # FLAG_DROP
+        if 'dropped the intelligence' in lline:
+            self.emit_event('tf2_flag_drop')
+        # POINT_CAPTURE
+        if 'captured point' in lline or 'captured control point' in lline:
+            self.emit_event('tf2_point_capture')
+        # ASSIST (brak bezpośrednio w logach serwerowych)
+        # RESPWAN (brak bezpośrednio, można próbować po "spawned" itd.)
+        # Inne eventy możesz dodać poniżej
 
     def emit_event(self, tf2_event, value=None):
         self.new_event.emit(tf2_event, value)
@@ -168,7 +165,6 @@ class EventMapWidget(QtWidgets.QWidget):
         self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
         self.combo_boxes = []
-        self.event_map = {}
 
         for row, (gs_name, gs_desc) in enumerate(GAMESENSE_EVENTS):
             item = QtWidgets.QTableWidgetItem(f"{gs_name}")
@@ -235,7 +231,7 @@ class MainWindow(QtWidgets.QTabWidget):
         super().__init__()
         self.setWindowTitle("TF2 GameSense Bridge")
         self.setWindowIcon(QtGui.QIcon("icon.ico"))
-        self.resize(700, 500)
+        self.resize(750, 500)
 
         # Zakładka 1 — ustawienia loga
         self.settings_tab = QtWidgets.QWidget()
@@ -243,22 +239,13 @@ class MainWindow(QtWidgets.QTabWidget):
         self.settings_tab.setLayout(settings_layout)
 
         form = QtWidgets.QFormLayout()
-        self.log_path_edit = QtWidgets.QLineEdit(DEFAULT_LOG_PATH)
-        self.browse_btn = QtWidgets.QPushButton("Wybierz...")
-        self.browse_btn.clicked.connect(self.choose_log_path)
+        self.logs_folder_edit = QtWidgets.QLineEdit(self.default_logs_folder())
+        self.browse_btn = QtWidgets.QPushButton("Wybierz folder...")
+        self.browse_btn.clicked.connect(self.choose_logs_folder)
         path_layout = QtWidgets.QHBoxLayout()
-        path_layout.addWidget(self.log_path_edit)
+        path_layout.addWidget(self.logs_folder_edit)
         path_layout.addWidget(self.browse_btn)
-        form.addRow("Plik logu TF2:", path_layout)
-
-        self.cmd_edit = QtWidgets.QLineEdit(self.gen_console_cmd())
-        self.cmd_edit.setReadOnly(True)
-        self.copy_btn = QtWidgets.QPushButton("Kopiuj")
-        self.copy_btn.clicked.connect(self.copy_cmd)
-        cmd_layout = QtWidgets.QHBoxLayout()
-        cmd_layout.addWidget(self.cmd_edit)
-        cmd_layout.addWidget(self.copy_btn)
-        form.addRow("Kod do konsoli TF2:", cmd_layout)
+        form.addRow("Folder z logami TF2:", path_layout)
 
         self.status_label = QtWidgets.QLabel("Niepołączony")
         self.status_dot = QtWidgets.QLabel("●")
@@ -267,6 +254,9 @@ class MainWindow(QtWidgets.QTabWidget):
         status_layout.addWidget(self.status_dot)
         status_layout.addWidget(self.status_label)
         form.addRow("Status GameSense:", status_layout)
+
+        self.nick_edit = QtWidgets.QLineEdit("")
+        form.addRow("Twój nick w TF2:", self.nick_edit)
 
         self.monitor_btn = QtWidgets.QPushButton("Start monitoring")
         self.monitor_btn.clicked.connect(self.toggle_monitor)
@@ -280,10 +270,9 @@ class MainWindow(QtWidgets.QTabWidget):
         self.timer.start(2000)
         self.check_gamesense()
 
-        self.log_path_edit.textChanged.connect(self.update_console_cmd)
-
         # Zakładka 2 — mapowanie eventów
         self.event_map_tab = EventMapWidget()
+        self.event_map_tab.load_mapping()
 
         self.addTab(self.settings_tab, "Ustawienia logów")
         self.addTab(self.event_map_tab, "Mapowanie eventów")
@@ -292,20 +281,23 @@ class MainWindow(QtWidgets.QTabWidget):
 
         self.monitor_thread = None
 
-    def choose_log_path(self):
-        fname, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Wybierz plik logu", self.log_path_edit.text(), "Text Files (*.txt);;All Files (*)")
-        if fname:
-            self.log_path_edit.setText(fname)
+    def default_logs_folder(self):
+        # Zgadujemy domyślną ścieżkę logs/ na podstawie typowych Steam Library
+        possible_paths = [
+            os.path.expandvars(r"%ProgramFiles(x86)%\Steam\steamapps\common\Team Fortress 2\tf\logs"),
+            os.path.expandvars(r"%ProgramFiles%\Steam\steamapps\common\Team Fortress 2\tf\logs"),
+            os.path.expanduser(r"~/Steam/steamapps/common/Team Fortress 2/tf/logs"),
+            r"d:\steamlibrary\steamapps\common\team fortress 2\tf\logs",
+        ]
+        for p in possible_paths:
+            if os.path.isdir(p):
+                return p
+        return os.path.expanduser("~/tf2logs")  # fallback
 
-    def gen_console_cmd(self):
-        path = self.log_path_edit.text().replace("\\", "\\\\")
-        return f'con_logfile "{path}"'
-
-    def update_console_cmd(self):
-        self.cmd_edit.setText(self.gen_console_cmd())
-
-    def copy_cmd(self):
-        QtWidgets.QApplication.clipboard().setText(self.cmd_edit.text())
+    def choose_logs_folder(self):
+        folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Wybierz folder z logami TF2", self.logs_folder_edit.text())
+        if folder:
+            self.logs_folder_edit.setText(folder)
 
     def check_gamesense(self):
         try:
@@ -330,14 +322,14 @@ class MainWindow(QtWidgets.QTabWidget):
                 self.monitor_thread = None
 
     def start_monitoring(self):
-        log_path = self.log_path_edit.text()
+        logs_folder = self.logs_folder_edit.text()
         event_map = self.event_map_tab.get_mapping()
-        self.monitor_thread = LogMonitorThread(log_path, event_map)
+        nick = self.nick_edit.text() or "Dawid"
+        self.monitor_thread = LogMonitorThread(logs_folder, event_map, nick)
         self.monitor_thread.new_event.connect(self.handle_new_event)
         self.monitor_thread.start()
 
     def handle_new_event(self, tf2_event, value):
-        # Znajdź na co zmapowany jest ten event w event_map.json
         event_map = self.event_map_tab.get_mapping()
         for gs_event, tf2_code in event_map.items():
             if tf2_code == tf2_event:
